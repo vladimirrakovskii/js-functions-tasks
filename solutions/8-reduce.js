15be51302a275f09f823843a3301717b81dd5df9
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:57:40 +0300

    Update 8-reduce.js

--- solutions/8-reduce.js
+++ solutions/8-reduce.js
@@ -1,3 +1,16 @@
 // BEGIN
-
+export default (objs, arg) => {
+    const res = {};
+    if (objs.length === 0 || arg === '') {
+        return res;
+    }
+    for (const obj of objs) {
+        const key = obj[arg];
+        if (!Object.hasOwn(res, key)) {
+            res[key] = [];
+        }
+        res[key].push(obj);
+    }
+    return res;
+}
 // END
