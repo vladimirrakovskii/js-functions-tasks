752bca3d6f11579b9d6b2a36db4c81246da00cd0
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 22:04:43 +0300

    Update 11-iterative.js

--- solutions/11-iterative.js
+++ solutions/11-iterative.js
@@ -1,6 +1,19 @@
 const smallestDivisor = (num) => {
     // BEGIN
-
+if (num === 1) {
+      return 1;
+    }
+    
+    const iter = (count) => {
+      if (count > Math.sqrt(num)) {
+        return num;
+      }
+      if (num % count === 0) {
+        return count;
+      }
+      return iter(count + 1);
+    }
+    return iter(2);
     // END
   };
   
