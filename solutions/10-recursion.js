c47fa0e8cad8928df1c7296768fbc1fd45953b1d
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 22:03:45 +0300

    Update 10-recursion.js

--- solutions/10-recursion.js
+++ solutions/10-recursion.js
@@ -1,6 +1,12 @@
 const sequenceSum = (begin, end) => {
     // BEGIN
-
+if (begin > end) {
+      return NaN;
+    }
+    if (begin === end) {
+      return end;
+    }
+    return begin + sequenceSum(begin + 1, end);
     // END
   };
   
