ecb78424cfefc61bf28f9520c85bf1ef3021cf0e
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:57:11 +0300

    Update 7-filter.js

--- solutions/7-filter.js
+++ solutions/7-filter.js
@@ -1,3 +1,6 @@
 // BEGIN
-
+export default (users) => {
+    const friendsGirl = users.map(user => user.friends.filter((friend) => friend.gender === 'female'));
+    return friendsGirl.flat();
+}
 // END
