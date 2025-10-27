7d02148b104abf4c7ae599010b841550d16d658c
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:47:57 +0300

    Update 4-first-class-citizen.js

--- solutions/4-first-class-citizen.js
+++ solutions/4-first-class-citizen.js
@@ -1,6 +1,16 @@
 const run = (text) => {
     // BEGIN
-
+const takeLast = (str, count) => {
+        if (str.length < count) {
+          return null;
+        }
+        if (str === '') {
+          return null;
+        }
+        const lastChars = str.slice(-count);
+        const res = lastChars.split('').reverse().join('');
+        return res;
+    }
     // END
   
     return takeLast(text, 4);
