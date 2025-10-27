ba5efbe9e3976f0d9482abb9c33face841321498
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:34:00 +0300

    Update 1-pure-functions.js

--- solutions/1-pure-functions.js
+++ solutions/1-pure-functions.js
@@ -1,3 +1,23 @@
 // BEGIN
+const isPrime = (num) => {
+    if (num <= 1) {
+        return false;
+    }
+    else 
+        for (let i = 2; i < num; i += 1) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+    return true;
+}
 
+export default (num) => {
+    if (isPrime(num)) {
+        console.log('yes');
+    }
+    else {
+        console.log('no');
+    }
+}
 // END
