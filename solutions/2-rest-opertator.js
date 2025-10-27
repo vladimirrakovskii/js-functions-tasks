aacd1e69c5af3080f3eb212c2273bffdce672d0b
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:34:54 +0300

    Update 2-rest-opertator.js

--- solutions/2-rest-opertator.js
+++ solutions/2-rest-opertator.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
 
 // BEGIN
-
+export default (...nums) => {
+    if (nums.length === 0) {
+        return null;
+    }
+    const res = _.sum(nums)/nums.length;
+    return res;
+}
 // END
