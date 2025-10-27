76a6b18da847b0f76dcffa53780bbc9ebb9b8d68
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:43:09 +0300

    Update 3-spread-operator.js

--- solutions/3-spread-operator.js
+++ solutions/3-spread-operator.js
@@ -1,3 +1,12 @@
 // BEGIN
+export default (...dates) => {
+  if (dates.length === 0) {
+    return [];
+  }
 
+  return dates.map(([year, month, day]) => {
+    const date = new Date(year, month - 1, day);
+    return date.toDateString();
+  });
+};
 // END
