83d336b732173afb92ac616895258c9783a89657
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:58:49 +0300

    Update 9-signals.js

--- solutions/9-signals.js
+++ solutions/9-signals.js
@@ -8,5 +8,21 @@ const freeEmailDomains = [
 ];
 
 // BEGIN
+export default (emails) => {
+  const res = {};
+  if (emails.length === 0) {
+    return res;
+  }
+  for (const email of emails) {
+    const domain = email.split('@')[1];
+    if (freeEmailDomains.includes(domain)) {
+      if (!Object.hasOwn(res, domain)) {
+        res[domain] = 0;
+      }
+      res[domain] += 1;
+    }
+  }
 
+  return res;
+}
 // END
