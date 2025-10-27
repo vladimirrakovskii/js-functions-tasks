43eab19828d9d8f2b76c37aa0c44f4a7dffdb16c
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 21:51:13 +0300

    Update 5-high-order-functions.js

--- solutions/5-high-order-functions.js
+++ solutions/5-high-order-functions.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
 // BEGIN
-
+export default (users, count = 1) => {
+    const sortUsers = _.sortBy(users, (user) => Date.parse(user.birthday));
+    const res = sortUsers.slice(0, count);
+    return res;
+}
 // END
